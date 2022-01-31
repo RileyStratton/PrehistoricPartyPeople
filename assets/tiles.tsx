84e3d464da6bc982ffb65ac3a33cbbd4f6412834
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-01-31 14:57:19 -0700

    added tsx

---
+++ assets/tiles.tsx
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tiles" tilewidth="129" tileheight="129" tilecount="18" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="129" height="129" source="4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="6.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="7.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="8.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="9.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="10.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="11.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="12.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="93" source="13.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="93" source="14.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="93" source="15.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="16.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="99" source="17.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="18.png"/>
+ </tile>
+</tileset>
