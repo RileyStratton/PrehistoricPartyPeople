5ae8202bde089d71d8ce30d6f4d3aa50694d0298
David Swearingen <83689213+Davids55@users.noreply.github.com>
2022-02-22 14:38:42 -0700

    Add files via upload

---
+++ assets/Decors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Decors" tilewidth="128" tileheight="128" tilecount="98" columns="14">
+ <image source="../../../Decors.png" width="1792" height="896"/>
+</tileset>
---
+++ assets/Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Tileset" tilewidth="128" tileheight="128" tilecount="48" columns="8">
+ <image source="../../../Tileset.png" width="1024" height="768"/>
+</tileset>
