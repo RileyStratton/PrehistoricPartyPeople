51278e86e3b5cb2c23209befa659b6aafdd08f4b
David Swearingen <captainD375@yahoo.com>
2022-03-22 14:43:45 -0600

    new map

---
+++ assets/tree.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="tree" tilewidth="32" tileheight="32" tilecount="18" columns="6">
+ <image source="tree.png" width="192" height="96"/>
+</tileset>
